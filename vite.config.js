8fbd34b8d5fb449a2e7442ac88a4c7400ea3848b
Vinicius Luz <vinnyowned@gmail.com>
2026-05-10 14:17:41 -0300

    Ajustando base path para /appbeta/

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Se o seu repositório for https://github.com/usuario/meu-app
-// O 'base' deve ser '/meu-app/'
-// Se for o domínio principal (usuario.github.io), o 'base' deve ser '/'
-
+// O 'base' deve ser o nome do seu repositório no GitHub entre barras
 export default defineConfig({
   plugins: [react()],
-  base: './', // O './' ajuda a funcionar em qualquer subpasta do GitHub Pages
+  base: '/appbeta/', 
 })
